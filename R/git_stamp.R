748855b110cdf9bd7406fc721ef22d8c5a1a3c96
christophergandrud <christopher.gandrud@gmail.com>
2014-11-08 17:48:43 +0100

    git_stamp added

--- R/git_stamp.R
+++ R/git_stamp.R
@@ -1,16 +1,17 @@
-#' Get git stamp (commit and branch) for the current code
+#' Get git stamp (commit and branch) for a repository
 #' 
-#' @param repo 
-#'  Git repo directory.
+#' @param repo Git repo directory. If unspecified, then the current working 
+#' directory is used.
 #'
 #' @description
-#'  The function returns the latest git commit and branch for the repo specified in \code{repo}.
+#'  The function returns the latest git commit and branch for the repo specified 
+#'  in \code{repo} or the current working directory if unspecified.
 #'  Git is needed for the command to run.
-#'  The functions makes it possible to include the latest git commit and branch in a run to
-#'  be able to know exactly which code where used.
+#'  The functions makes it possible to include the latest git commit and branch 
+#'  in a run to be able to know exactly which code where used.
 #'
 #' @return 
-#'  character vector with latest commit and branch
+#  character vector with latest commit and branch
 #'
 #' @example
 #' \dontrun{
@@ -22,9 +23,8 @@ git_stamp <- function(repo = getwd()){
   setwd(repo)
   git_commit <- system(paste0("git rev-parse --verify HEAD"), intern = TRUE)
   git_branch <- system(paste0("git rev-parse --abbrev-ref HEAD"), intern = TRUE)
-  git_message <- system(paste0("git log --format=%B -n 1 ", git_commit), intern = TRUE)[1]
+  git_message <- system(paste0("git log --format=%B -n 1 ", git_commit), 
+                        intern = TRUE)[1]
   setwd(start_wd)
-  c("commit" = git_commit, "branch"=git_branch, "message"=git_message)
+  c("commit" = git_commit, "branch" = git_branch, "message" = git_message)
 }
-
-
